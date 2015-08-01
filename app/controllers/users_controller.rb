794e83e288092e3ef2b2b0e9e6bf8761d60fea9b
Christian Barentine <cbarentine@gmail.com>
2015-08-01 11:36:39 -0500

    Make sure user gets logged in when directed to the dashboard after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login_user
       redirect_to dashboard_path
     else
       # display validation error message
