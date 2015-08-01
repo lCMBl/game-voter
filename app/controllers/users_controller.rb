4a3200c0184b064fdfd7342fd443a87beb393b8c
Christian Barentine <cbarentine@gmail.com>
2015-08-01 11:34:56 -0500

    Added user creation route, and validation for email uniqueness

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+    render new_user_path
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to dashboard_path
+    else
+      # display validation error message
+      render new_user_path
+    end
   end
 
   def dashboard
@@ -14,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-
+    params.require(:user).permit(:email, :name, :password)
   end
 end
