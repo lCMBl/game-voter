f473749780e97266d03162bd066007af68181b18
Christian Barentine <cbarentine@gmail.com>
2015-08-03 10:34:36 -0500

    added appropriate queries to dashboard controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  include SessionHelpers
+  before_action :check_authentication
+  skip_before_action :check_authentication, except: [:dashboard]
+
   def new
     @user = User.new
     render new_user_path
@@ -17,6 +21,14 @@ class UsersController < ApplicationController
 
   def dashboard
     # displays all relevant game idea data
+    @user = current_user
+    @leading_games = Game.select("games.*, COUNT(votes.id) vote_count").joins(:votes).group("games.id").order("vote_count DESC").limit(5)
+    @user_games = @user.games
+    @games_in_vote_que = []
+    Game.all.each do |game|
+      @games_in_vote_que << game if Vote.where(game_id: game.id, user_id: current_user_id).count == 0
+    end
+    # @games_in_vote_que = Game.select("games.*").joins(:votes).group("games.id").where.not(user_id: @user.id)
   end
 
   private
