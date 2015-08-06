4758f7c3457a5c6bed5eec6c5872e557b827941e
Christian Barentine <cbarentine@gmail.com>
2015-08-06 13:14:30 -0500

    updated test to reflect styling change

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -9,7 +9,7 @@ describe SessionsController do
     fill_in('user_email', with: correct_login[:email])
     fill_in('user_password', with: correct_login[:password])
     click_on("Log in")
-    expect(page).to have_content("test's Dashboard")
+    expect(page).to have_content("Logged in as: test")
     expect(current_path).to eq(dashboard_path)
   end
 end
